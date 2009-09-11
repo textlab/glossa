f770ed06a390397f9665b62791cbaa4751a49a47
Anders Nøklestad <anders.noklestad@iln.uio.no>
2009-09-11 15:22:18 +0200

    Tentative file structure for (parts of) search interface

---
+++ public/javascripts/search/search_panel.js
@@ -0,0 +1,22 @@
+/**
+ * @author Anders Nøklestad
+ */
+
+// For JSLint
+/*global Ext, app, urlRoot */
+
+// MODULE DESCRIPTION HERE
+
+app.search.SearchPanel = function() {
+    // private properties
+
+    // private functions
+
+    return {
+        // public properties
+        main: {
+        }
+
+        // public functions
+    };
+}();
