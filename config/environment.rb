761433f991a60d39f1ad62ef44bad924589539c3
Anders Nøklestad <anders.noklestad@iln.uio.no>
2009-12-03 01:41:57 +0100

    Updated to Rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
   config.gem "binarylogic-authlogic", :source => "http://gems.github.com", :lib => "authlogic"
   config.gem "be9-acl9", :source => "http://gems.github.com", :lib => "acl9"
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
-  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"  
+  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
