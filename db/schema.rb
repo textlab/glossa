80e3378555be0adb941d1ae5f938a6b137debaf8
Anders Nøklestad <anders.noklestad@iln.uio.no>
2009-12-10 10:51:53 +0100

    Created the join table for the CorpusText and Subcorpus habtm relation

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091210093443) do
+ActiveRecord::Schema.define(:version => 20091210094952) do
 
   create_table "corpora", :force => true do |t|
     t.string   "name",       :null => false
