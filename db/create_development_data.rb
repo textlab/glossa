e5287868e890b451e51e18488f43de273b4bc855
Anders Nøklestad <anders.noklestad@iln.uio.no>
2009-12-24 04:17:02 +0100

    More development data

--- db/create_development_data.rb
+++ db/create_development_data.rb
@@ -1,3 +1,28 @@
 if RAILS_ENV == 'development'
   corpus = Corpus.create!(:name => 'The Oslo Corpus of Tagged Norwegian Texts')
+
+  corpus.metadata_categories.create!(:name => 'title', :fieldtype => 'text')
+  corpus.metadata_categories.create!(:name => 'author', :fieldtype => 'text')
+  corpus.metadata_categories.create!(:name => 'publishing_year', :fieldtype => 'integer')
+
+  language_config_type = LanguageConfigType.create!(
+          :name => "Norwegian bokmål, written, Constraint Grammar",
+          :tagger => 'obt'
+  )
+  config = corpus.language_configs.create!(:language_config_type => language_config_type)
+
+  text = config.corpus_texts.create!(:uri => 'peer_gynt.txt')
+
+  title = TextMetadataValue.create!(:corpus_text => text, :metadata_category => MetadataCategory.find_by_name('title'))
+  title.value = 'Peer Gynt'
+  title.save!
+
+  author = TextMetadataValue.create!(:corpus_text => text, :metadata_category => MetadataCategory.find_by_name('author'))
+  author.value = 'Henrik Ibsen'
+  author.save!
+
+  year = IntegerMetadataValue.create!(:corpus_text => text, :metadata_category => MetadataCategory.find_by_name('publishing_year'))
+  year.value = 1867
+  year.save!
+
 end
