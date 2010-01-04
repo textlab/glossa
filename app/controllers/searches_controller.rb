05dde8f54778c256930a528c8fa1fc341f6d4e7b
Anders Nøklestad <anders.noklestad@iln.uio.no>
2010-01-04 18:14:02 +0100

    Added some SearchesController specs

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,17 @@
 class SearchesController < ApplicationController
   def create
+    unless params.has_key?(:data)
+      render :json => {:success => false, :message => 'Data parameter is missing'}, :status => :bad_request
+      return
+    end
+
+    unless params[:data].is_a?(Hash) && params[:data].has_key?(:queries)
+      render :json => {:success => false, :message => 'Queries parameter is missing'}, :status => :bad_request
+      return
+    end
+
+#    unless params[:data].has_ke
+#    current_user.searches.create!(params[:data]) if current_user
   end
 
   def destroy
--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,24 +1,53 @@
 require 'spec_helper'
 
 describe SearchesController do
-
-  #Delete these examples and add some real ones
-  it "should use SearchesController" do
-    controller.should be_an_instance_of(SearchesController)
-  end
-
-
   describe "POST 'create'" do
-    it "should be successful" do
-      post 'create'
-      response.should be_success
-    end
-  end
 
-  describe "POST 'destroy'" do
-#    it "should be successful" do
-#      delete '/'
-#      response.should be_success
+    describe "if all parameters are correctly set" do
+      it "should be successful" do
+        post 'create', :data => {:queries => SAMPLE_SEARCH_DATA[:queries]}
+        response.should be_success
+      end
+    end
+
+    describe "if params[:data] is not set" do
+      before do
+        post 'create'
+      end
+
+      it "response status should be 'Bad Request'" do
+        response.status.should include('Bad Request')
+      end
+
+      it "response message should say that the data parameter is missing" do
+        resp = JSON.parse(response.body)
+        resp['message'].should have_text('Data parameter is missing')
+      end
+    end
+
+    describe "if params[:data][:queries] is not set" do
+      before do
+        post 'create', :data => {}
+      end
+
+      it "response status should be 'Bad Request'" do
+        response.status.should include('Bad Request')
+      end
+    end
+
+#    describe "if no user is logged in" do
+#      it "should not create a Search object" do
+#  #      Search.delete_all
+#        Search.count.should == 0
+#      end
+#    end
+#
+#    describe "if a user is logged in" do
+#      it "should create a Search object" do
+#  #      Search.delete_all
+#        post 'create'
+#        Search.count.should == 1
+#      end
 #    end
   end
 end
