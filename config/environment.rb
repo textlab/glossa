798d39664fd5b2e15646938515af13cf7881bb25
Joel Priestley <joeljp@hf-162-240.uio.no>
2010-01-05 14:23:40 +0100

    Fixed gem names

--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "binarylogic-authlogic", :source => "http://gems.github.com", :lib => "authlogic"
-  config.gem "be9-acl9", :source => "http://gems.github.com", :lib => "acl9"
+  config.gem "authlogic"
+  config.gem "acl9"
   config.gem "rspec", :lib => false, :version => ">= 1.2.0"
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
 
