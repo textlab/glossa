95af6dd6389d7176c8c91c78ea54f0c27d94a876
Anders Nøklestad <anders.noklestad@iln.uio.no>
2010-05-11 22:08:00 +0200

    Dynamic addition and removal of query terms

--- public/javascripts/App/Search/LanguageBar.ui.js
+++ public/javascripts/App/Search/LanguageBar.ui.js
@@ -1,11 +1,40 @@
 App.Search.LanguageBarUi = Ext.extend(Ext.Panel, {
     height: 140,
     layout: 'hbox',
+    layoutConfig: {
+      align: 'middle'
+    },
     padding: '10px',
     autoHeight: false,
     tbar: [],
     initComponent: function() {
+        this.items = [
+            {
+                xtype: 'container',
+                autoEl: 'div',
+                flex: 1,
+                style: 'margin-left: 10px',
+                defaults: {
+                  xtype: 'button',
+                  cls: 'x-btn-text-icon',
+                  width: 95
+                },
+                items: [
+                    {
+                        text: 'Add word&nbsp&nbsp;&nbsp;&nbsp;',
+                        icon: urlRoot + 'images/add.png',
+                        ref: '../addWordButton',
+                        style: 'margin-bottom: 5px'
+                    },
+                    {
+                        text: 'Delete word',
+                        icon: urlRoot + 'images/delete.png',
+                        ref: '../deleteWordButton',
+                        disabled: true
+                    }
+                ]
+            }
+        ];
         App.Search.LanguageBarUi.superclass.initComponent.call(this);
     }
 });
- 
