c1940b085b03fd3534cd7f71fc601b278c1beb12
Anders Nøklestad <anders.noklestad@iln.uio.no>
2010-05-26 17:15:02 +0200

    Autoexpand the (currently single) column in results grid

--- public/javascripts/App/Search/SearchResultsPanel.ui.js
+++ public/javascripts/App/Search/SearchResultsPanel.ui.js
@@ -1,28 +1,29 @@
 App.Search.SearchResultsPanelUi = Ext.extend(Ext.grid.GridPanel, {
   title: 'Search results',
   columns: [{
-			xtype: 'templatecolumn',
-			header: 'Result',
-			dataIndex: 'line',
-			tpl: new Ext.XTemplate('<tpl for="line">{.}<br></tpl>'),
-			width: 650
-	}],
-	store: new Ext.data.JsonStore({
-		fields: ['line'],
- 		url: urlRoot + 'searches/query',
-		totalProperty: 'querySize',
-		root: 'data'
-	}),
-	bbar: {
+    id: 'line',
+    xtype: 'templatecolumn',
+    header: 'Result',
+    dataIndex: 'line',
+    tpl: new Ext.XTemplate('<tpl for="line">{.}<br></tpl>')
+  }],
+  autoExpandColumn: 'line',
+  store: new Ext.data.JsonStore({
+    fields: ['line'],
+    url: urlRoot + 'searches/query',
+    totalProperty: 'querySize',
+    root: 'data'
+  }),
+  bbar: {
     xtype: 'paging'
   },
 
   initComponent: function() {
     App.Search.SearchResultsPanelUi.superclass.initComponent.call(this);
 
-		// connect pager to the grids datastore
-		// bindStore() undocumented ?
-		this.getBottomToolbar().bindStore(this.getStore());
-		this.getBottomToolbar().pageSize = App.Controller.resultPagerSize;
+    // connect pager to the grids datastore
+    // bindStore() undocumented ?
+    this.getBottomToolbar().bindStore(this.getStore());
+    this.getBottomToolbar().pageSize = App.Controller.resultPagerSize;
   }
 });
