72640208a8912681e334da4aa5a761c6e33ba676
Anders Nøklestad <anders.noklestad@iln.uio.no>
2011-04-14 20:51:39 +0200

    More work on the Permissions module

--- app/controllers/permissions.rb
+++ app/controllers/permissions.rb
@@ -3,11 +3,11 @@
 # Author: Anders Nøklestad
 
 # Include this module into controllers that need access control. The module
-# provides the controller and its subclasses with three class methods:
-# +default_allowed+, +allow_for+, and +deny_for+. Each of these methods takes
-# one or more role names as its argument(s). In addition to names of actual
-# roles, the methods accept the following pseudo-role names: :all, :none,
-# :logged_in, and :anonymous (i.e. not logged in).
+# provides the controller and its subclasses with four class methods:
+# +default_allowed+, +allow_for+, +also_allow_for+, and +deny_for+. Each of
+# these methods takes one or more role names as its argument(s). In addition
+# to names of actual roles, the methods accept the following pseudo-role names:
+# :all, :none, :logged_in, and :anonymous (i.e. not logged in).
 #
 # When the module is included into a controller, its default behaviour
 # is to deny everybody access to all actions in the controller and all
@@ -16,16 +16,16 @@
 # application will be protected. From there on you can use the +default_allowed+
 # method at the top of a controller class definition to specify a set of roles that
 # should have access to all the actions in the controller by default, and you can
-# use the +allow_for+ and +deny_for+ decorator-like methods to specify
-# access to individual actions (see examples below).
+# use the +allow_for+, +also_allow_for+, and +deny_for+ decorator-like methods to
+# specify access to individual actions (see examples below).
 
 # One common scenario is that you have an intranet application that should mostly
 # only be accessible for logged-in users, but there may be one or more
 # areas that should be open to anonymous users. You would then call
 # "default_allowed :logged_in" in your ApplicationController and
 # "default_allowed :anonymous" in those controllers that handle publicly
-# available areas. +allow_for+ and +deny_for+ may additionally be used to regulate
-# access to individual actions in any of the controllers.
+# available areas. +allow_for+, +also_allow_for+, and +deny_for+ may additionally
+# be used to regulate access to individual actions in any of the controllers.
 #
 # Another common scenario is that you have a site which is mostly open to the public
 # but contains certain areas that should only be accessible to logged-in users or
@@ -34,19 +34,19 @@
 # "default_allowed :logged_in", "default_allowed :admin", "allow_for :admin" etc.
 # to protect certain controllers or individual actions in your controller subclasses.
 #
-# Note that a call to +allow_for+ restricts access to the listed roles, regardless
-# of the default permissions of the controller. In other words, even if you have
-# specified "default_allowed :all" at the top of your controller, a call to
+# Note that a call to +allow_for+ restricts the set of allowed roles to the ones given,
+# regardless of the default permissions of the controller. In other words, even if you
+# have specified "default_allowed :all" at the top of your controller, a call to
 # "allow_for :admin" will still deny access to the following action to anyone but
-# admins.
+# admins. +also_allow_for+, on the other hand, adds the given set of roles to the
+# ones defined by +default_allowed+ (in the controller or one of its ancestors).
 #
-# On the other hand, +deny_for+ is only useful if the list of roles given is more
-# restrictive than the default set of allowed roles for the controller. Thus, if
-# you have specified "default_allowed :admin" for your controller, a call to
-# "deny_for :member" does not have any effect, since the +member+ role was not part
-# of the default set of allowed roles anyway. In other words, a call to +deny_for+
-# does not automatically grant access to all roles that are not listed in the
-# +deny_for+ call!
+# +deny_for+ is only useful if the list of roles given is more restrictive than the
+# default set of allowed roles for the controller. Thus, if you have specified
+# "default_allowed :admin" for your controller, a call to "deny_for :member" does
+# not have any effect, since the +member+ role was not part of the default set of
+# allowed roles anyway. In other words, a call to +deny_for+ does not automatically
+# grant access to all roles that are not listed in the +deny_for+ call!
 #
 # As an example, let us look at some of the controllers you might define as part of
 # a forum application:
@@ -89,7 +89,8 @@
 #
 # # This controller handles administrative tasks, which should by default only
 # # be accessible to admins and superadmins - some even only to superadmins. However,
-# # even this controller may contain actions that should be accessible to anyone.
+# # even this controller may contain actions that should be accessible to additional
+# # roles or even anonymous users.
 # class AdminController < ApplicationController
 #
 #   default_allowed :admin, :superadmin  # overrides the default permissions that were
@@ -105,77 +106,117 @@
 #     ....
 #   end
 #
+#   also_allow_for :member    # accessible to members as well as admins and superadmins
+#   def edit_preferences
+#     ....
+#   end
+#
 # end
 #
 
 module Permissions
+
+  # Some useful role sets
+  NOBODY = Set.new([:none])
+  EVERYBODY = Set.new([:anonymous, :all])
+
   def self.included(base)
     base.class_eval do
-      # +default_perm+ needs to be inherited from the superclass unless it
-      # is specifically set for a particular controller. Class variables won't
-      # work, since their values are shared by the entire class hierarchy, but
-      # Rails' convenient +class_attribute+ method is perfect for this.
-      class_attribute :default_perm
-      self.default_perm = :deny
+      # +def_allowed+ needs to be inherited from the superclass unless it
+      # is specifically set for a particular controller using a call to
+      # +default_allowed+. Class variables won't work, since their values
+      # are shared by the entire class hierarchy, but Rails' convenient
+      # +class_attribute+ method is perfect for this.
+      class_attribute :def_allowed
+      self.def_allowed = NOBODY    # defaults to being maximally restrictive
     end
     base.extend ClassMethods
+
+    # Register the filter that will do the actual access control checking
     base.send(:before_filter, :check_action_permissions)
   end
 
+  def check_action_permissions
+    allowed_roles = self.class.allowed_roles_for(params[:action])
+
+    if current_user.nil?
+      # User is not logged in
+      denied = (allowed_roles & EVERYBODY).empty?
+    else
+      # User is logged in
+      denied = (allowed_roles & Set.new([:logged_in, current_user.role.name.to_sym])).empty?
+    end
+
+    raise AccessDenied if denied
+
+  end
+
   module ClassMethods
-    def default_permission(permission)
-      self.default_perm = permission
+    # Sets the set of roles that can access all actions in this controller and
+    # its subclasses unless overridden by an +allow_for+ or +deny_for+ call or
+    # extended by an +also_allowed_for+ call for individual actions.
+    def default_allowed(*roles)
+      self.def_allowed = set_with(roles)
     end
 
+    # Sets the set of roles that can access the following action regardless of
+    # the default setting for the controller.
     def allow_for(*roles)
-      @latest_allowed = Array(roles).flatten.map(&:to_sym)  # will be used by the +method_added+ hook
+      @latest_allowed = set_with(roles)   # will be used by the +method_added+ hook
     end
 
+    # Extends the set of roles that can access the following action by adding the
+    # given roles to the default set of allowed roles for the controller.
+    def also_allow_for(*roles)
+      @latest_also_allowed = set_with(roles)   # will be used by the +method_added+ hook
+    end
+
+    # Restricts the set of roles that can access the following action. The roles
+    # listed here will be subtracted from the default set of allowed roles for
+    # the controller.
     def deny_for(*roles)
-      @latest_denied = Array(roles).flatten.map(&:to_sym)   # will be used by the +method_added+ hook
+      @latest_denied = set_with(roles)   # will be used by the +method_added+ hook
     end
 
     def method_added(method_name)
-      # +allowed_roles+ and +denied_roles+ are specific to each individual
-      # controller and should not be inherited. They are therefore suitably
-      # defined as instance variables on the class. Furthermore, they cannot
-      # be defined by Permissions#included, since the module will only be
-      # included into the base controller. Hence, we should define them here
-      # as needed.
+      # +@allowed_roles+ is specific to each individual controller and should
+      # not be inherited. It is therefore suitably defined as an instance
+      # variable on the class. Furthermore, it cannot be defined by
+      # Permissions#included, since the module will only be included into the
+      # base controller. Hence, we should define it here as needed.
       @allowed_roles ||= {}
 
       # Calculate the set of roles that are allowed to call this action, based
-      # on default_perm and the set of roles given to the immediately preceding
-      # call to +allow_for+ (if any).
-      @allowed_roles[method_name] = allowed_set(@latest_allowed)
+      # on the default set of allowed roles for the controller and the set of
+      # roles given to any preceding call to +allow_for+, +also_allow_for+, or
+      # +deny_for+.
+      @allowed_roles[method_name.to_sym] = calculate_allowed_set
 
-      # Make sure any roles set by +allow_for+ are not carried over to the next action.
-      @latest_allowed = nil
+      # Make sure any roles set by +allow_for+, +also_allow_for+, or +deny_for+
+      # are not carried over to the next action.
+      @latest_allowed = @latest_also_allowed = @latest_denied = nil
+    end
 
-      # Same procedure for denied roles.
-      @denied_roles ||= {}
-      @denied_roles[method_name] = denied_set(@latest_denied)
-      @latest_denied = nil
+    def allowed_roles_for(action)
+      @allowed_roles[action.to_sym] || Set.new([:none])
     end
 
     ########
     private
     ########
 
-    def allowed_set(allowed_for_method)
-      if self.default_perm == :allow
-        allowed_for_method ? allowed_for_method.to_set : Set.new([:all])
-      end
+    def set_with(roles)
+      Set.new(Array(roles).flatten.map(&:to_sym))
     end
 
-    def denied_set(denied_for_method)
+    def calculate_allowed_set
+      return @latest_allowed if @latest_allowed
+      return def_allowed - @latest_denied if @latest_denied
+      return def_allowed + @latest_also_allowed if @latest_also_allowed
+      def_allowed
     end
   end
 
-  ########
-  private
-  ########
+  class AccessDenied < StandardError; end
 
-  def check_action_permissions
-  end
 end
