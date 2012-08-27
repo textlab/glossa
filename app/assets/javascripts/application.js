dfaea5c59a4c18116b04184faca406ae74e25406
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-08-27 22:18:21 +0200

    Require Bootstrap files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,5 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
+//= require jquery
+//= require bootstrap-2.1.0/bootstrap
