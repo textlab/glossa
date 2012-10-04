08637c0a41ca8a71ea9c31f271a3c64775882745
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-10-04 17:49:48 +0200

    Move development data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,33 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+language_config_type = LanguageConfigType.create!(
+  :name => "Norwegian bokmål, written, The Oslo-Bergen Tagger",
+  :tagger => 'obt'
+)
+corpus = Corpus.create!(:name => 'The Oslo Corpus of Tagged Norwegian Texts')
+
+corpus.metadata_categories.create!(:name => 'title', :fieldtype => 'text')
+corpus.metadata_categories.create!(:name => 'author', :fieldtype => 'text')
+corpus.metadata_categories.create!(:name => 'publishing_year', :fieldtype => 'integer')
+
+# language_config_type = LanguageConfigType.create!(
+#   :name => "Norwegian bokmål, written, The Oslo-Bergen Tagger",
+#   :tagger => 'obt'
+#   )
+# config = corpus.language_configs.create!(:language_config_type => language_config_type)
+
+# text = config.corpus_texts.create!(:uri => 'peer_gynt.txt')
+
+title = MetadataValueText.create!(:metadata_category => MetadataCategory.find_by_name('title'))
+title.value = 'Peer Gynt'
+title.save!
+
+author = MetadataValueText.create!(:metadata_category => MetadataCategory.find_by_name('author'))
+author.value = 'Henrik Ibsen'
+author.save!
+
+year = MetadataValueInteger.create!(:metadata_category => MetadataCategory.find_by_name('publishing_year'))
+year.value = 1867
+year.save!
