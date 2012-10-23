bff85d8b9c85dd57a0d92acf3ffc1e5431b7f236
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-10-23 15:37:37 +0200

    Create LocalizationSupport module

--- app/models/corpus.rb
+++ app/models/corpus.rb
@@ -1,5 +1,11 @@
 class Corpus < ActiveRecord::Base
+  include LocalizationSupport
+
   has_many :metadata_categories, dependent: :destroy, order: :name
 
   validates_presence_of :name
+
+  def name
+    localized_attribute(:name)
+  end
 end
--- app/models/metadata_category.rb
+++ app/models/metadata_category.rb
@@ -1,4 +1,6 @@
 class MetadataCategory < ActiveRecord::Base
+  include LocalizationSupport
+
   belongs_to :corpus
   has_many   :metadata_values, dependent: :destroy, order: :text_value
 
@@ -7,9 +9,6 @@ class MetadataCategory < ActiveRecord::Base
   validates_presence_of :value_type
 
   def name
-    # Names that start with a colon should be localized; others should just be
-    # returned as is
-    name_attr = read_attribute(:name)
-    name_attr.start_with?(':') ? I18n.t(name_attr[1..-1]) : name_attr
+    localized_attribute(:name)
   end
 end
