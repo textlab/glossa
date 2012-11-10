cf96803051ab41e0ca2bb08da377b5145919bf35
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-11-10 01:56:08 +0100

    Method that selects corpus texts based on metadata values

--- app/models/corpus_text.rb
+++ app/models/corpus_text.rb
@@ -1,3 +1,16 @@
 class CorpusText < ActiveRecord::Base
   has_and_belongs_to_many :metadata_values
+
+  class << self
+    # Returns all corpus texts that are associated with the metadata values
+    # that have the given database ids
+    def matching_metadata(metadata_value_ids)
+        CorpusText
+        .joins('INNER JOIN corpus_texts_metadata_values ON ' +
+          'corpus_texts_metadata_values.corpus_text_id = corpus_texts.id')
+        .where(
+          corpus_texts_metadata_values: { metadata_value_id: metadata_value_ids }
+        )
+    end
+  end
 end
