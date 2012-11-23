b3fef30bd3d9d0c1ed5b844de52d3a40acfbcc0d
Anders Nøklestad <anders.noklestad@iln.uio.no>
2012-11-23 14:20:03 +0100

    Rename routes
    Avoid the name "start" for a route, since it has special meaning to the state manager and causes additional, unwanted routing

--- app/controllers/search_types/cwb_searches_controller.rb
+++ app/controllers/search_types/cwb_searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchTypes::CwbSearchesController < SearchesController
 
-  # Used by the base controller to find the right kind of model to manipulate
+  # Used by the base controller to find the right kind of model to work with
   def model
     SearchTypes::CwbSearch
   end
