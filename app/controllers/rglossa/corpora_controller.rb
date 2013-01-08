74ff5d7232b396b3e6425979b65d7f7f20d8b34c
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-01-08 18:02:54 +0100

    Support query params in call to /corpora

--- app/controllers/rglossa/corpora_controller.rb
+++ app/controllers/rglossa/corpora_controller.rb
@@ -3,7 +3,9 @@ module Rglossa
     respond_to :json, :xml
 
     def index
-      @corpora = Corpus.all
+      @corpora = Corpus.scoped
+      set_query_params
+
       respond_with @corpora
     end
 
@@ -14,6 +16,19 @@ module Rglossa
         .find(params[:id])
 
       respond_with @corpus
+    end
+
+    ########
+    private
+    ########
+
+    def set_query_params
+      query = params[:query]
+      if query
+        query.each_pair do |key, value|
+          @corpora = @corpora.where(key.underscore.to_sym => value)
+        end
+      end
     end
 
   end
