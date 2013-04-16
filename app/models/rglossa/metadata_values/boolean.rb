5675e15517defd8bd0eabf5df1c1d6401e54a054
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-04-16 15:46:18 +0200

    Formatting

--- app/models/rglossa/metadata_values/boolean.rb
+++ app/models/rglossa/metadata_values/boolean.rb
@@ -6,11 +6,11 @@ module Rglossa
       end
 
       def text=(val)
-      # Set the "text" of this metadata value to *true* if the given value is
-      # true and it is not one of the strings "no" and "false"; otherwise, set
-      # it to false
-      self.boolean_value = val && !val.in?('no', 'false')
+        # Set the "text" of this metadata value to *true* if the given value is
+        # true and it is not one of the strings "no" and "false"; otherwise, set
+        # it to false
+        self.boolean_value = val && !val.in?('no', 'false')
+      end
     end
   end
-end
 end
