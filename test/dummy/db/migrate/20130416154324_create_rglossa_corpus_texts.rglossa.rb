cfc8716cf83789f64209bff8da921d71444f1b78
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-04-17 00:44:09 +0200

    Allow startpos and endpos to be initially empty

--- test/dummy/db/migrate/20130416154324_create_rglossa_corpus_texts.rglossa.rb
+++ test/dummy/db/migrate/20130416154324_create_rglossa_corpus_texts.rglossa.rb
@@ -2,8 +2,8 @@
 class CreateRglossaCorpusTexts < ActiveRecord::Migration
   def change
     create_table :rglossa_corpus_texts do |t|
-      t.integer :startpos, null: false, limit: 8  # limit = 8 means bigint
-      t.integer :endpos,   null: false, limit: 8
+      t.integer :startpos, limit: 8  # limit = 8 means bigint
+      t.integer :endpos,   limit: 8
 
       t.timestamps
     end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318185620) do
+ActiveRecord::Schema.define(:version => 20130416154328) do
 
   create_table "rglossa_corpora", :force => true do |t|
     t.string   "name",             :null => false
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(:version => 20130318185620) do
   end
 
   create_table "rglossa_corpus_texts", :force => true do |t|
-    t.integer  "startpos",   :limit => 8, :null => false
-    t.integer  "endpos",     :limit => 8, :null => false
+    t.integer  "startpos",   :limit => 8
+    t.integer  "endpos",     :limit => 8
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
   end
@@ -106,23 +106,5 @@ ActiveRecord::Schema.define(:version => 20130318185620) do
 
   add_index "rglossa_users", ["email"], :name => "index_rglossa_users_on_email", :unique => true
   add_index "rglossa_users", ["reset_password_token"], :name => "index_rglossa_users_on_reset_password_token", :unique => true
-
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
