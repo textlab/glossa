206cacc3948677eefae567aa39dbf233370d045b
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-04-22 14:55:52 +0200

    Add CorpusText.by_tid to easily find a text by its text id

--- app/models/rglossa/corpus_text.rb
+++ app/models/rglossa/corpus_text.rb
@@ -6,11 +6,18 @@ module Rglossa
 
     class << self
 
+      def by_tid(tid)
+        joins('INNER JOIN rglossa_corpus_texts_metadata_values j ON ' +
+                   'j.rglossa_corpus_text_id = rglossa_corpus_texts.id')
+        .joins('INNER JOIN rglossa_metadata_values v ON j.rglossa_metadata_value_id = v.id')
+        .joins('INNER JOIN rglossa_metadata_categories c ON v.metadata_category_id = c.id')
+        .where(c: {short_name: 'tid'}, v: { text_value: tid })
+      end
+
       # Returns all corpus texts that are associated with the metadata values
       # that have the given database ids
       def matching_metadata(metadata_value_ids)
-        CorpusText
-        .select('startpos, endpos').uniq
+        select('startpos, endpos').uniq
         .joins('INNER JOIN rglossa_corpus_texts_metadata_values j ON ' +
           'j.rglossa_corpus_text_id = rglossa_corpus_texts.id')
         .where(j: { rglossa_metadata_value_id: metadata_value_ids })
