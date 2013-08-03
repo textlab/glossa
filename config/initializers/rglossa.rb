63bb47407aae21ed1e484eb4f0b680dd5505c448
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-08-03 23:11:45 +0200

    Read config files with tags at startup

--- config/initializers/rglossa.rb
+++ config/initializers/rglossa.rb
@@ -2,3 +2,17 @@ require 'i18n-js'
 
 Rails.application.config.assets.paths <<
   SimplesIdeias::I18n::Engine.root.join('vendor', 'assets', 'javascripts').to_s
+
+
+module Rglossa
+  @taggers = {}
+
+  Dir.glob(Rglossa::Engine.root.join('config', 'taggers', '*')) do |filename|
+    tagger = File.basename(filename, '.json')
+    @taggers[tagger] = JSON.parse(File.read(filename))
+  end
+
+  def self.taggers
+    @taggers
+  end
+end
