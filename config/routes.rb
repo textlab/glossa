f041de62f2e67dd5028e64687584d253fdce73fb
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-09 00:20:29 +0200

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,6 @@ Rglossa::Engine.routes.draw do
     # Add more search types to the resource list as they are implemented, e.g.
     # resources :cwb_searches, :corpuscle_searches, :annis2_searches do
     resources :cwb_searches do
-      collection do
-        post 'query'
-
-        # FIXME: These don't belong here
-        get 'corpora_list'
-        get 'corpus_info'
-      end
-
       member do
         get 'results'
       end
