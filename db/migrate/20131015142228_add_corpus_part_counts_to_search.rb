cf5b359af65cd6da84720b6828cce10bfb48c4e5
Anders Nøklestad <anders.noklestad@iln.uio.no>
2014-09-08 17:04:21 +0200

    MySQL doesn't accept default value for text col

--- db/migrate/20131015142228_add_corpus_part_counts_to_search.rb
+++ db/migrate/20131015142228_add_corpus_part_counts_to_search.rb
@@ -1,5 +1,5 @@
 class AddCorpusPartCountsToSearch < ActiveRecord::Migration
   def change
-    add_column :rglossa_searches, :corpus_part_counts, :text, null: false, default: ''
+    add_column :rglossa_searches, :corpus_part_counts, :text, null: false
   end
 end
