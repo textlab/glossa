ed991b994caa0d755c5363507791a4857b1b57fe
Michał Kosek <mihao@users.noreply.github.com>
2014-09-25 18:05:21 +0200

    Indentation changes

--- app/controllers/rglossa/speech/waveform_player_controller.rb
+++ app/controllers/rglossa/speech/waveform_player_controller.rb
@@ -1,151 +1,153 @@
 module Rglossa
-  module Speech
-    class WaveformPlayerController < ActionController::Base
-      class <<self
-        def before_start
-          width = @conf['pixels_per_second'] * 60
-          height = @conf['total_height']
-          # The display number is the same as the port number
-          opts = {:display => @conf['listen_port'], :dimensions => "#{width}x#{height}x16"}
+module Speech
 
-          # Start Xvfb again if it already exists, as the dimensions may have changed
-          headless = begin
-            Headless.new(opts.merge({:reuse => false}))
-          rescue Headless::Exception
-            Headless.new(opts.merge({:reuse => true})).destroy
-            Headless.new(opts.merge({:reuse => false}))
-          end
-          headless.start
-        end
+class WaveformPlayerController < ActionController::Base
+  class <<self
+    def before_start
+      width = @conf['pixels_per_second'] * 60
+      height = @conf['total_height']
+      # The display number is the same as the port number
+      opts = {:display => @conf['listen_port'], :dimensions => "#{width}x#{height}x16"}
 
-        def update_conf?
-          !@server_controller || !@server_controller.running?
-        end
-
-        def update_conf
-          @conf = WaveformController.conf
-        end
-
-        def conf
-          !update_conf? && @conf ? @conf : update_conf
-        end
-
-        def server_controller
-          if update_conf?
-            update_conf
-            pid_file = Rails.root.join(@conf['pid_file']).to_s
-            log_file = Rails.root.join(@conf['log_file']).to_s
-            @server_controller = DaemonController.new(
-              :identifier => 'Waveform server',
-              :before_start => method(:before_start),
-              :start_command =>
-                'cd %s && exec %s %s %s >>%s 2>&1' %
-                  [Rails.public_path, Rglossa::Engine.root.join('lib/waveforms/genwaveform.py').to_s,
-                   WaveformPlayer.conf_path, pid_file, log_file].
-                  map {|s| Shellwords.escape s },
-              :daemonize_for_me => true,
-              :ping_command => [:tcp, '127.0.0.1', @conf['listen_port']],
-              :pid_file => pid_file,
-              :log_file => log_file
-            )
-          else
-            @server_controller
-          end
-        end
+      # Start Xvfb again if it already exists, as the dimensions may have changed
+      headless = begin
+        Headless.new(opts.merge({:reuse => false}))
+      rescue Headless::Exception
+        Headless.new(opts.merge({:reuse => true})).destroy
+        Headless.new(opts.merge({:reuse => false}))
       end
+      headless.start
+    end
 
-      def old_glossa_filename(file, corp)
-        file.gsub!(/^[A-Z]*_/, "")
-        file.gsub!(/\.wav/i, "")
-        file = "BB_" + file.gsub(/_[A-Z]+$/, "") if corp == 'bigbrother'
-        file
-      end
+    def update_conf?
+      !@server_controller || !@server_controller.running?
+    end
 
-      def show
-        @conf = self.class.conf
-        output_path = Rails.root.join(Rails.public_path, @conf['output_dir'])
-        Dir.mkdir output_path unless Dir.exists? output_path
-        corpus_id = params[:corpus_id]
-        line_key = params[:line_key]
-        start = params[:start]
-        stop = params[:stop]
-        @file = Digest::SHA256.hexdigest("#{corpus_id}@#{line_key}@#{start}@#{stop}");
-        fname = Rails.root.join(output_path, @file)
-        if (!File.exists?("#{fname}-0-wave.jpg"))
-          if corpus_id.match(/^\d+$/) #new Glossa
-            corpus = Corpus.find_by_id(corpus_id)
-            movie_loc = "%s_800.mp4" % corpus.media_files.where("line_key_begin <= ? AND ? <= line_key_end",
-                                                                line_key, line_key).limit(1).first.basename.rstrip
-            local_path = Rails.root.join(Rails.public_path, corpus.media_path || "media/#{corpus.short_name}", "audio")
+    def update_conf
+      @conf = WaveformController.conf
+    end
 
-            start_time = sprintf("%d.%02d.%d", start.to_i / 60, start.to_i % 60, start.gsub(/^[^.]*(\.|$)/, "").to_i)
-            stop_time = sprintf("%d.%02d.%d", stop.to_i / 60, stop.to_i % 60, stop.gsub(/^[^.]*(\.|$)/, "").to_i)
-            Process.wait spawn("mp3splt", Rails.root.join(local_path, movie_loc).to_s, start_time, stop_time,
-                               "-d", output_path.to_s, "-o", @file, {:in => :close})
-            if !File.exists? "#{fname}.wav"
-              Process.wait spawn("ffmpeg", "-i", "#{fname}.mp3", "-ac", "1", "#{fname}.wav", {:in => :close})
-            end
-          elsif corpus_id.match(/^[a-z]+$/) #old Glossa
-            conn = ActiveRecord::Base.establish_connection("oldglossa").connection
-            puts("SELECT audio_file FROM %ssegments WHERE id=%d LIMIT 1" %
-                               [corpus_id.upcase, line_key.to_i])
-            res = conn.execute("SELECT audio_file FROM %ssegments WHERE id=%d LIMIT 1" %
-                               [corpus_id.upcase, line_key.to_i])
-            basename = res.first.first
-            ActiveRecord::Base.establish_connection(ENV["RAILS_ENV"])
+    def conf
+      !update_conf? && @conf ? @conf : update_conf
+    end
 
-            path = "rtmp://stream-prod01.uio.no/vod/mp4:uio/hf/ilf/#{corpus_id}/"
-            glossa_fn = old_glossa_filename(basename, corpus_id)
-            movie_loc = "mp4:uio/hf/ilf/#{corpus_id}/audio/#{glossa_fn}.mp4"
-            if (!File.exists?("#{fname}.flv") || File.size("#{fname}.flv") == 0)
-              Process.wait spawn("rtmpdump", "-r", path, "-y", movie_loc, "-A", start,
-                                 "-B", stop, "-o", "#{fname}.flv")
-            end
-            if !File.exists? "#{fname}.wav"
-              Process.wait spawn("ffmpeg", "-i", "#{fname}.flv", "-ac", "1", "#{fname}.wav", {:in => :close})
-            end
-            if !File.exists? "#{fname}.mp3"
-              Process.wait spawn("lame", "--quiet", "#{fname}.wav", "#{fname}.mp3")
-            end
-          end
-
-          fp = self.class.server_controller.connect do
-            TCPSocket.new '127.0.0.1', @conf['listen_port']
-          end
-          fp.write("#{@file}.wav\n")
-          until fp.eof
-            fp.read
-          end
-          fp.close
-        end
-
-        i = 0
-        @basenames = []
-        while File.exists?("#{fname}-#{i}-pitch.png")
-          @basenames.push "#{@file}-#{i}"
-          i += 1
-        end
-        @basenames.each do |basename|
-          FileUtils.touch "#{output_path}/#{basename}-wave.jpg"
-          FileUtils.touch "#{output_path}/#{basename}-fmt.png"
-          FileUtils.touch "#{output_path}/#{basename}-pitch.png"
-        end
-        FileUtils.touch("#{fname}.mp3")
-
-        FileUtils.rm_f "#{fname}.wav"
-        FileUtils.rm_f "#{fname}.flv"
-
-        max_i = 0
-        @total_imgwidth = 0
-        @imgwidths = []
-        i = 0
-        @basenames.each do |basename|
-          max_i = i
-          sz = FastImage.size("#{output_path}/#{basename}-pitch.png")
-          @total_imgwidth += sz[0]
-          @imgwidths[i] = sz[0]
-        end
+    def server_controller
+      if update_conf?
+        update_conf
+        pid_file = Rails.root.join(@conf['pid_file']).to_s
+        log_file = Rails.root.join(@conf['log_file']).to_s
+        @server_controller = DaemonController.new(
+          :identifier => 'Waveform server',
+          :before_start => method(:before_start),
+          :start_command =>
+            'cd %s && exec %s %s %s >>%s 2>&1' %
+              [Rails.public_path, Rglossa::Engine.root.join('lib/waveforms/genwaveform.py').to_s,
+               WaveformPlayer.conf_path, pid_file, log_file].
+              map {|s| Shellwords.escape s },
+          :daemonize_for_me => true,
+          :ping_command => [:tcp, '127.0.0.1', @conf['listen_port']],
+          :pid_file => pid_file,
+          :log_file => log_file
+        )
+      else
+        @server_controller
       end
     end
   end
+
+  def old_glossa_filename(file, corp)
+    file.gsub!(/^[A-Z]*_/, "")
+    file.gsub!(/\.wav/i, "")
+    file = "BB_" + file.gsub(/_[A-Z]+$/, "") if corp == 'bigbrother'
+    file
+  end
+
+  def show
+    @conf = self.class.conf
+    output_path = Rails.root.join(Rails.public_path, @conf['output_dir'])
+    Dir.mkdir output_path unless Dir.exists? output_path
+    corpus_id = params[:corpus_id]
+    line_key = params[:line_key]
+    start = params[:start]
+    stop = params[:stop]
+    @file = Digest::SHA256.hexdigest("#{corpus_id}@#{line_key}@#{start}@#{stop}");
+    fname = Rails.root.join(output_path, @file)
+    if (!File.exists?("#{fname}-0-wave.jpg"))
+      if corpus_id.match(/^\d+$/) #new Glossa
+        corpus = Corpus.find_by_id(corpus_id)
+        movie_loc = "%s_800.mp4" % corpus.media_files.where("line_key_begin <= ? AND ? <= line_key_end",
+                                                            line_key, line_key).limit(1).first.basename.rstrip
+        local_path = Rails.root.join(Rails.public_path, corpus.media_path || "media/#{corpus.short_name}", "audio")
+
+        start_time = sprintf("%d.%02d.%d", start.to_i / 60, start.to_i % 60, start.gsub(/^[^.]*(\.|$)/, "").to_i)
+        stop_time = sprintf("%d.%02d.%d", stop.to_i / 60, stop.to_i % 60, stop.gsub(/^[^.]*(\.|$)/, "").to_i)
+        Process.wait spawn("mp3splt", Rails.root.join(local_path, movie_loc).to_s, start_time, stop_time,
+                           "-d", output_path.to_s, "-o", @file, {:in => :close})
+        if !File.exists? "#{fname}.wav"
+          Process.wait spawn("ffmpeg", "-i", "#{fname}.mp3", "-ac", "1", "#{fname}.wav", {:in => :close})
+        end
+      elsif corpus_id.match(/^[a-z]+$/) #old Glossa
+        conn = ActiveRecord::Base.establish_connection("oldglossa").connection
+        puts("SELECT audio_file FROM %ssegments WHERE id=%d LIMIT 1" %
+                           [corpus_id.upcase, line_key.to_i])
+        res = conn.execute("SELECT audio_file FROM %ssegments WHERE id=%d LIMIT 1" %
+                           [corpus_id.upcase, line_key.to_i])
+        basename = res.first.first
+        ActiveRecord::Base.establish_connection(ENV["RAILS_ENV"])
+
+        path = "rtmp://stream-prod01.uio.no/vod/mp4:uio/hf/ilf/#{corpus_id}/"
+        glossa_fn = old_glossa_filename(basename, corpus_id)
+        movie_loc = "mp4:uio/hf/ilf/#{corpus_id}/audio/#{glossa_fn}.mp4"
+        if (!File.exists?("#{fname}.flv") || File.size("#{fname}.flv") == 0)
+          Process.wait spawn("rtmpdump", "-r", path, "-y", movie_loc, "-A", start,
+                             "-B", stop, "-o", "#{fname}.flv")
+        end
+        if !File.exists? "#{fname}.wav"
+          Process.wait spawn("ffmpeg", "-i", "#{fname}.flv", "-ac", "1", "#{fname}.wav", {:in => :close})
+        end
+        if !File.exists? "#{fname}.mp3"
+          Process.wait spawn("lame", "--quiet", "#{fname}.wav", "#{fname}.mp3")
+        end
+      end
+
+      fp = self.class.server_controller.connect do
+        TCPSocket.new '127.0.0.1', @conf['listen_port']
+      end
+      fp.write("#{@file}.wav\n")
+      until fp.eof
+        fp.read
+      end
+      fp.close
+    end
+
+    i = 0
+    @basenames = []
+    while File.exists?("#{fname}-#{i}-pitch.png")
+      @basenames.push "#{@file}-#{i}"
+      i += 1
+    end
+    @basenames.each do |basename|
+      FileUtils.touch "#{output_path}/#{basename}-wave.jpg"
+      FileUtils.touch "#{output_path}/#{basename}-fmt.png"
+      FileUtils.touch "#{output_path}/#{basename}-pitch.png"
+    end
+    FileUtils.touch("#{fname}.mp3")
+
+    FileUtils.rm_f "#{fname}.wav"
+    FileUtils.rm_f "#{fname}.flv"
+
+    max_i = 0
+    @total_imgwidth = 0
+    @imgwidths = []
+    i = 0
+    @basenames.each do |basename|
+      max_i = i
+      sz = FastImage.size("#{output_path}/#{basename}-pitch.png")
+      @total_imgwidth += sz[0]
+      @imgwidths[i] = sz[0]
+    end
+  end
+end
+
+end
 end
--- lib/tasks/rglossa_tasks.rake
+++ lib/tasks/rglossa_tasks.rake
@@ -1,49 +1,47 @@
 namespace "rglossa" do
-  namespace "install" do
+namespace "install" do
 
-    desc "Install thor scripts"
-    task :thor do
-      print "Installing thor scripts... "
+desc "Install thor scripts"
+task :thor do
+  print "Installing thor scripts... "
 
-      filenames = "rglossa_*.thor"
-      source = Dir["#{Rglossa::Engine.root}/lib/tasks/#{filenames}"]
-      dest   = "#{Rails.root}/lib/tasks"
-      old_files = Dir["#{dest}/#{filenames}"]
+  filenames = "rglossa_*.thor"
+  source = Dir["#{Rglossa::Engine.root}/lib/tasks/#{filenames}"]
+  dest   = "#{Rails.root}/lib/tasks"
+  old_files = Dir["#{dest}/#{filenames}"]
 
-      FileUtils.rm(old_files)
-      FileUtils.cp(source, dest)
+  FileUtils.rm(old_files)
+  FileUtils.cp(source, dest)
 
-      puts "done."
-    end
+  puts "done."
+end
 
+desc "Copy files to application"
+task :copy_files do
+  print "Copying layout file..."
 
-    desc "Copy files to application"
-    task :copy_files do
-      print "Copying layout file..."
+  source = Dir["#{Rglossa::Engine.root}/app/views/layouts/application.html.erb"]
+  dest   = "#{Rails.root}/app/views/layouts"
+  FileUtils.cp(source, dest)
 
-      source = Dir["#{Rglossa::Engine.root}/app/views/layouts/application.html.erb"]
-      dest   = "#{Rails.root}/app/views/layouts"
-      FileUtils.cp(source, dest)
+  puts "done."
 
-      puts "done."
+  print "Copying application.js..."
 
-      print "Copying application.js..."
+  source = Dir["#{Rglossa::Engine.root}/lib/copy_to_app/application.js"]
+  dest   = "#{Rails.root}/app/assets/javascripts"
+  FileUtils.cp(source, dest)
 
-      source = Dir["#{Rglossa::Engine.root}/lib/copy_to_app/application.js"]
-      dest   = "#{Rails.root}/app/assets/javascripts"
-      FileUtils.cp(source, dest)
+  puts "done."
+end
 
-      puts "done."
-    end
+desc "Download, compile and install the Snack Sound Toolkit"
+task :snack => :environment do
+  conf = Rglossa::Speech::WaveformPlayer.conf
+  Dir.chdir Rglossa::Engine.root.join('lib/waveforms')
+  Process.wait spawn("./install_snack.sh", conf['snack_dir'], conf['tcl_dir'],
+                     conf['tk_dir'], Dir.mktmpdir)
+end
 
-    desc "Download, compile and install the Snack Sound Toolkit"
-    task :snack => :environment do
-      conf = Rglossa::Speech::WaveformPlayer.conf
-      Dir.chdir Rglossa::Engine.root.join('lib/waveforms')
-      Process.wait spawn("./install_snack.sh", conf['snack_dir'], conf['tcl_dir'],
-                         conf['tk_dir'], Dir.mktmpdir)
-    end
-
-  end
-
+end
 end
