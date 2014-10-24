8bcd20c503df5c3ee8e0d3c5ea90da5ca10b8e7e
Anders Nøklestad <anders.noklestad@iln.uio.no>
2014-10-24 18:00:37 +0200

    Add onchange handler for simple view input

--- src/cljs/cglossa/search_inputs.cljs
+++ src/cljs/cglossa/search_inputs.cljs
@@ -2,13 +2,17 @@
   (:require [clojure.string :as str]))
 
 (defn cwb-search-inputs [{:keys [search-query]}]
-  (let [displayed-query (-> @search-query
-                            :query
-                            (str/replace #"\[\(?\w+=\"(.+?)\"(?:\s+%c)?\)?\]" "$1"))]
+  (let [query (:query @search-query)
+        displayed-query (str/replace query #"\[\(?\w+=\"(.+?)\"(?:\s+%c)?\)?\]" "$1")
+        phonetic? (not= -1 (.indexOf query "phon="))]
     [:div.row-fluid
-    [:form.form-inline.span12
-     [:div.span10
-      [:input.span12 {:ref "searchfield" :type "text" :value displayed-query}]]]]))
+     [:form.form-inline.span12
+      [:div.span10
+       [:input.span12 {:type "text" :value displayed-query
+                       :on-change #(swap! search-query assoc-in [:query] (aget % "target" "value"))}]
+       [:label {:style {:marginTop 5}}]
+       [:input {:name "phonetic" :type "checkbox"
+                :style {:marginTop -3} :checked phonetic?} " Phonetic form"]]]]))
 
 (def components {:cwb        cwb-search-inputs
                  :cwb-speech (fn [] [:div "CWB-SPEECHE"])})
