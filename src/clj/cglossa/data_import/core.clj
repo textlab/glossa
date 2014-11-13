11e020f626134f21e4cb50507f321202f5015359
Anders Nøklestad <anders.noklestad@iln.uio.no>
2014-11-13 03:50:21 +0100

    Rename parameter

--- src/clj/cglossa/data_import/core.clj
+++ src/clj/cglossa/data_import/core.clj
@@ -9,13 +9,13 @@
        str/split-lines
        (map #(str/split % #"\t"))))
 
-(defn tsv->tx-data [path model]
+(defn tsv->tx-data [path namespace]
   "Takes as input a tsv (tab-separated values) file with attribute names
   on the first line and the actual model data on the following lines.
   Returns a Datomic transaction data seq that can be fed directly into
   datomic.api/transact."
   (let [rows (tsv->rows path)
-        attr-names (->> (first rows) (map #(keyword (str model "/" %))))
+        attr-names (->> (first rows) (map #(keyword (name namespace) %)))
         data (rest rows)]
     (for [row data]
       (into {:db/id (tempid :db.part/user)} (zipmap attr-names row)))))
