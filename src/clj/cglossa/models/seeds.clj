0796580dcfe78df0b879b0c02316d814fda9297d
Anders Nøklestad <anders.noklestad@iln.uio.no>
2014-11-13 18:44:20 +0100

    Fix metadata import

--- src/clj/cglossa/models/seeds.clj
+++ src/clj/cglossa/models/seeds.clj
@@ -14,21 +14,22 @@
 (defn import-corpora []
   (tsv->tx-data "resources/data/corpora.tsv" :corpus))
 
-(defn import-metadata []
+(defn import-metadata-categories []
   (->> (find-tsv-files "resources/data/metadata_categories")
        (map #(tsv->tx-data % :metadata-category))
-       (map (fn [file-metadata]
-              (let [path (-> file-metadata meta :from-path)
-                    short-name (last (re-find #".+/(.+).tsv$" path))
-                    corpus-attr {:corpus/_metadata-categories [:corpus/short-name short-name]}]
-                ; associate the metadata with the appropriate corpus by adding
-                ; an attribute with a corpus lookup ref to each metadata category
-                (map #(into % corpus-attr) file-metadata))))))
+       (mapcat (fn [file-metadata]
+                 (let [path (-> file-metadata meta :from-path)
+                       short-name (last (re-find #".+/(.+).tsv$" path))
+                       corpus-attr {:corpus/_metadata-categories [:corpus/short-name short-name]}]
+                   ; associate the metadata with the appropriate corpus by adding
+                   ; an attribute with a corpus lookup ref to each metadata category
+                   (map #(into % corpus-attr) file-metadata))))))
 
 (defn seed []
   (let [url models/db-uri
         conn (d/connect url)]
     (d/transact conn (concat
-                     (generate-parts d/tempid (models/dbschema))
-                     (generate-schema d/tempid (models/dbschema))))
-    (d/transact conn (import-corpora))))
+                       (generate-parts d/tempid (models/dbparts))
+                       (generate-schema d/tempid (models/dbschema))))
+    (d/transact conn (import-corpora))
+    (d/transact conn (import-metadata-categories))))
