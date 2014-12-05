de807c0146cbeade998d358137fb14375d485d14
Michał Kosek <mihao@users.noreply.github.com>
2014-12-05 17:50:47 +0100

    Make two images: glossa-base contains prerequisites, and glossa contains our code.

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-bundle update rglossa rglossa-r rglossa-fcs
+set -e
+bundle update --source rglossa rglossa-r rglossa-fcs
 rake railties:install:migrations   # installs migrations for all engines
 rake rglossa:install:thor
-docker build -t textlab/glossa .
 
+rm_dockerfile() {
+  if [ -h Dockerfile ]; then rm -f Dockerfile; fi
+}
+
+set +e
+if [ "$1" = "--full" ]; then
+  rm_dockerfile
+  ln -s Dockerfile.glossa-base Dockerfile || exit 1
+  docker build -t textlab/glossa-base . || { rm -f Dockerfile; exit 1; }
+fi
+rm_dockerfile
+ln -s Dockerfile.glossa Dockerfile || exit 1
+docker build -t textlab/glossa . || { rm -f Dockerfile; exit 1; }
+rm_dockerfile
