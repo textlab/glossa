4e38acd1a2b201cf41cd809fbc0bb35371ce2aa0
Michał Kosek <mihao@users.noreply.github.com>
2015-03-31 17:18:47 +0200

    Redirect to the front page if corpus is not specified

--- app/controllers/rglossa/home_controller.rb
+++ app/controllers/rglossa/home_controller.rb
@@ -6,7 +6,9 @@ module Rglossa
       if Rails.env == 'development' && params.has_key?(:user)
         session[:current_username] = params[:user]
       end
+      unless params[:corpus].present?
+        redirect_to front_url
+      end
     end
-
   end
 end
