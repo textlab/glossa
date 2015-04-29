ae8e777caa6dbbe9bf99f6cde660ae20ed56b8b4
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-04-29 18:18:57 +0200

    Fix db schema

--- src/clj/cglossa/models/schema.clj
+++ src/clj/cglossa/models/schema.clj
@@ -9,23 +9,23 @@
            (fields
              [name :string]
              [short-name :string :unique-identity]
-             [metadata-categories :ref :many :component :index]))
+             [metadata-categories :ref :many :component :indexed]))
 
    (schema metadata-category
            (fields
-             [short-name :string :index]
+             [short-name :string :indexed]
              [name :string]
              [widget-type :enum [:list :range]]
-             [values :ref :many :index]))
+             [values :ref :many :indexed]))
 
    (schema metadata-value
            (fields
              ; connect the metadata value to one or more text IDs (i.e.
              ; metadata values in the 'tid' category)
-             [tids :ref :many :index]
-             [text-value :string :index]
-             [numeric-value :long :index]
-             [bool-value :boolean :index]))
+             [tids :ref :many :indexed]
+             [text-value :string :indexed]
+             [numeric-value :long :indexed]
+             [bool-value :boolean :indexed]))
 
    (schema search
            (fields
