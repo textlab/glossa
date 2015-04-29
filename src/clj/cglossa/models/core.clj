db5a4741b578964ef86f47dea318c79bbe66e564
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-04-29 18:21:27 +0200

    WIP on Datomic fetches

--- src/clj/cglossa/models/core.clj
+++ src/clj/cglossa/models/core.clj
@@ -1,6 +1,8 @@
 (ns cglossa.models.core
   (:require [datomic.api :as d]))
 
+(def db-uri "datomic:free://localhost:4334/glossa")
+;(def db-uri "datomic:mem://localhost:4334/glossa")
 
 (defn current-db []
   (d/db (d/connect db-uri)))
@@ -13,3 +15,23 @@
   ([attr value db]
    (let [ids (d/q '[:find ?e :in $ ?a ?v :where [?e ?a ?v]] db attr value)]
      (d/entity db (ffirst ids)))))
+
+(defn extract-pulled-ids [data]
+  "Takes a potentially nested hash map resulting from a Datomic pull
+  and extracts all :db/id attribute values from it."
+  (->> (for [[k v] data]
+         (cond
+           (= k :db/id) v                        ; return the id
+           (map? v) (extract-pulled-ids v)))     ; process nested map
+       flatten                                   ; flatten result of nested maps
+       (filter identity)))                       ; remove nils
+
+(defn get-updates [db since ids]
+  (sort (d/q '[:find ?tx ?added ?e ?attr ?val
+               :in $ $since [?e ...]
+               :where
+               [$since ?e ?a ?val ?tx ?added]
+               [?a :db/ident ?attr]]
+             db
+             (d/history (d/since db since))
+             ids)))
