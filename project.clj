8d9e30abecb75a8a0d1b8b4316431a6830484f9d
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-05-21 15:51:27 +0200

    Formatting

--- project.clj
+++ project.clj
@@ -1,85 +1,85 @@
 (defproject cglossa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :license {:name "Eclipse Public License"
+                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :source-paths ["src/clj"]
+            :source-paths ["src/clj"]
 
-  :test-paths ["spec/clj"]
+            :test-paths ["spec/clj"]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211" :scope "provided"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [ring "1.3.2"]
-                 [ring/ring-defaults "0.1.4"]
-                 [ring-middleware-format "0.5.0"]
-                 [compojure "1.3.3"]
-                 [enlive "1.1.5"]
-                 [reagent "0.5.0"]
-                 [environ "1.0.0"]
-                 [http-kit "2.1.19"]
-                 [prismatic/plumbing "0.4.2" :exclusions [potemkin]]
-                 [cljs-ajax "0.3.11"]
-                 [prone "0.8.1"]
-                 [com.orientechnologies/orientdb-graphdb "2.0.8"]
-                 [clojurewerkz/ogre "2.5.0.0"]]
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [org.clojure/clojurescript "0.0-3211" :scope "provided"]
+                           [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                           [ring "1.3.2"]
+                           [ring/ring-defaults "0.1.4"]
+                           [ring-middleware-format "0.5.0"]
+                           [compojure "1.3.3"]
+                           [enlive "1.1.5"]
+                           [reagent "0.5.0"]
+                           [environ "1.0.0"]
+                           [http-kit "2.1.19"]
+                           [prismatic/plumbing "0.4.2" :exclusions [potemkin]]
+                           [cljs-ajax "0.3.11"]
+                           [prone "0.8.1"]
+                           [com.orientechnologies/orientdb-graphdb "2.0.8"]
+                           [clojurewerkz/ogre "2.5.0.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-environ "1.0.0"]
-            [lein-sassc "0.10.0"]
-            [lein-auto "0.1.1"]]
+            :plugins [[lein-cljsbuild "1.0.6"]
+                      [lein-environ "1.0.0"]
+                      [lein-sassc "0.10.0"]
+                      [lein-auto "0.1.1"]]
 
-  :min-lein-version "2.5.0"
+            :min-lein-version "2.5.0"
 
-  :uberjar-name "cglossa.jar"
+            :uberjar-name "cglossa.jar"
 
-  :jvm-opts ^:replace ["-Xmx1g" "-server"]
+            :jvm-opts ^:replace ["-Xmx1g" "-server"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/out"]
+            :clean-targets ^{:protect false} ["resources/public/js/out"]
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs" "target/classes"]
-                             :compiler {:output-to     "resources/public/js/out/app.js"
-                                        :output-dir    "resources/public/js/out"
-                                        :source-map    "resources/public/js/out/out.js.map"
-                                        :source-map-timestamp true
-                                        :optimizations :none
-                                        :cache-analysis true
-                                        :main "cglossa.core"
-                                        :asset-path "js/out"
-                                        :pretty-print  true}}}}
+            :cljsbuild {:builds {:app {:source-paths ["src/cljs" "target/classes"]
+                                       :compiler     {:output-to            "resources/public/js/out/app.js"
+                                                      :output-dir           "resources/public/js/out"
+                                                      :source-map           "resources/public/js/out/out.js.map"
+                                                      :source-map-timestamp true
+                                                      :optimizations        :none
+                                                      :cache-analysis       true
+                                                      :main                 "cglossa.core"
+                                                      :asset-path           "js/out"
+                                                      :pretty-print         true}}}}
 
-  :sassc [{:src "src/scss/style.scss"
-           :output-to "resources/public/css/style.css"}]
-  :auto {"sassc"  {:file-pattern  #"\.(scss)$"}}
+            :sassc [{:src       "src/scss/style.scss"
+                     :output-to "resources/public/css/style.css"}]
+            :auto {"sassc" {:file-pattern #"\.(scss)$"}}
 
-  :profiles {:dev {:source-paths ["env/dev/clj"]
+            :profiles {:dev     {:source-paths ["env/dev/clj"]
 
-                   :dependencies [[figwheel "0.2.7"]
-                                  [com.cemerick/piggieback "0.2.1"]
-                                  [org.clojure/tools.nrepl "0.2.10"]]
+                                 :dependencies [[figwheel "0.2.7"]
+                                                [com.cemerick/piggieback "0.2.1"]
+                                                [org.clojure/tools.nrepl "0.2.10"]]
 
-                   :repl-options {:init-ns cglossa.server
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                                 :repl-options {:init-ns          cglossa.server
+                                                :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-                   :plugins [[lein-figwheel "0.2.7"]]
+                                 :plugins      [[lein-figwheel "0.2.7"]]
 
-                   :figwheel {:css-dirs ["resources/public/css"]
-                              :open-file-command "idea-opener"}
+                                 :figwheel     {:css-dirs          ["resources/public/css"]
+                                                :open-file-command "idea-opener"}
 
-                   :env {:is-dev true}
+                                 :env          {:is-dev true}
 
-                   :cljsbuild {:builds
-                               {:app
-                                {:source-paths ["env/dev/cljs"]}}}}
+                                 :cljsbuild    {:builds
+                                                {:app
+                                                 {:source-paths ["env/dev/cljs"]}}}}
 
-             :uberjar {:source-paths ["env/prod/clj"]
-                       :hooks [leiningen.cljsbuild  ]
-                       :env {:production true}
-                       :omit-source true
-                       :aot :all
-                       :cljsbuild {:builds {:app
-                                            {:source-paths ["env/prod/cljs"]
-                                             :compiler
-                                                           {:optimizations :advanced
-                                                            :pretty-print false}}}}}})
+                       :uberjar {:source-paths ["env/prod/clj"]
+                                 :hooks        [leiningen.cljsbuild]
+                                 :env          {:production true}
+                                 :omit-source  true
+                                 :aot          :all
+                                 :cljsbuild    {:builds {:app
+                                                         {:source-paths ["env/prod/cljs"]
+                                                          :compiler
+                                                                        {:optimizations :advanced
+                                                                         :pretty-print  false}}}}}})
