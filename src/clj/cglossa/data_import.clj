d7e572a8ad46fc64b180757dbf6cf8d307974a83
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-05-24 20:04:26 +0200

    WIP on data import

--- src/clj/cglossa/data_import.clj
+++ src/clj/cglossa/data_import.clj
@@ -6,17 +6,37 @@
             [me.raynes.conch :as conch]
             [me.raynes.fs :as fs]))
 
-(defn import-corpora []
-  (let [tsv-path    "resources/data/corpora.tsv"
-        etl-path    (:oetl environ/env)
-        config-path (.getPath (fs/absolute "resources/data/corpus.json"))]
+(defn run-etl [config-path]
+  (let [etl-path (:oetl environ/env)]
     (assert etl-path (str "Please set the OETL environment variable to the path "
                           "to the OrientDB script oetl.sh."))
+    (conch/let-programs [etl etl-path]
+                        (etl config-path {:seq true}))))
+
+(defn import-corpora []
+  (let [tsv-path    "resources/data/corpora.tsv"
+        config-path (.getPath (fs/absolute "resources/data/corpora.json"))]
     (with-open [file (io/reader tsv-path)]
       (let [[headers & data] (csv/read-csv file :separator \tab)]
-        (assert (= ["name" "code"] headers)
+        (assert (= ["code" "name"] headers)
                 (str "Format error: Expected first line to contain column headers "
-                     "and the two columns to be 'name' and 'code'."))
-        (println etl-path)
-        (conch/let-programs [etl etl-path]
-                            (etl config-path {:seq true}))))))
+                     "and the two columns to be 'code' and 'name'."))
+        (run-etl config-path)))))
+
+(defn import-metadata-categories [corpus]
+  (let [tsv-path        (-> (str "resources/data/metadata_categories/" corpus ".tsv")
+                            fs/absolute
+                            .getPath)
+        config-template (.getPath (fs/absolute "resources/data/metadata_categories.json"))
+        config-file     (fs/temp-file "metadata_cat_config")
+        config          (-> (slurp config-template)
+                            (str/replace "###CORPUS###" corpus)
+                            (str/replace "###TSV-PATH###" tsv-path))]
+    (println config-file)
+    (spit config-file config)
+    (with-open [file (io/reader tsv-path)]
+      (let [[headers & data] (csv/read-csv file :separator \tab)]
+        (assert (= ["code" "name"] headers)
+                (str "Format error: Expected first line to contain column headers "
+                     "and the two columns to be 'code' and 'name'."))
+        (run-etl config-file)))))
