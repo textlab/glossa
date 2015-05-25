d5b05f4901e529f94737184e2f84dcbf3fef5a24
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-05-25 20:25:39 +0200

    WIP on metadata value import

--- src/clj/cglossa/data_import.clj
+++ src/clj/cglossa/data_import.clj
@@ -20,9 +20,7 @@
    :loader       {:orientdb
                   {:dbURL   "remote:localhost/Glossa",
                    :dbType  "graph",
-                   :classes [{:name "Corpus", :extends "V"}
-                             {:name "MetadataCategory", :extends "V"}
-                             {:name "HasMetadataCategory", :extends "E"}],
+                   :classes [{:name "Corpus", :extends "V"}],
                    :indexes [{:class "Corpus", :fields ["code:string"], :type "UNIQUE"}]}}})
 
 (def ^:private metadata-categories-config-template
@@ -47,7 +45,39 @@
                    :classes [{:name "Corpus", :extends "V"}
                              {:name "MetadataCategory", :extends "V"}
                              {:name "HasMetadataCategory", :extends "E"}],
-                   :indexes [{:class "Corpus", :fields ["code:string"], :type "UNIQUE"}]}}})
+                   :indexes [{:class "Corpus", :fields ["code:string"], :type "UNIQUE"}
+                             {:class "MetadataCategory", :fields ["code:string"], :type "UNIQUE"}
+                             {:class "MetadataCategory", :fields ["corpus_cat:string"], :type "UNIQUE"}]}}})
+
+(def ^:private tids-config-template
+  {:config       {:log "debug"},
+   :begin        [{:console
+                   {:commands
+                    ["CONNECT remote:localhost/Glossa admin admin;"
+                     "TRUNCATE CLASS MetadataValue;"
+                     "TRUNCATE CLASS HasMetadataValue;"]}}],
+   :source       {:file {:path :WILL-BE-REPLACED}},
+   :extractor    {:row {}},
+   :transformers [{:csv {:separator "\t"}}
+                  {:vertex {:class "MetadataValue"}}
+                  {:edge
+                   {:class                "HasMetadataValue",
+                    :lookup               "MetadataCategory.corpus_cat",
+                    :joinValue            :WILL-BE-REPLACED
+                    :unresolvedLinkAction "ERROR"}}],
+   :loader       {:orientdb
+                  {:dbURL   "remote:localhost/Glossa",
+                   :dbType  "graph",
+                   :classes [{:name "MetadataCategory", :extends "V"}
+                             {:name "MetadataValue", :extends "V"}
+                             {:name "HasMetadataValue", :extends "E"}],
+                   :indexes [{:class "MetadataCategory", :fields ["corpus_cat:string"], :type "UNIQUE"}]}}})
+
+(defn- read-csv [file]
+  (csv/read-csv file :separator \tab :quote \^))
+
+(defn- write-csv [file data]
+  (csv/write-csv file data :separator \tab))
 
 (defn- run-etl [config-path]
   (let [etl-path (:oetl environ/env)]
@@ -72,12 +102,12 @@
       ;; the category combined, which will be used to create edges from
       ;; the category to its values when we import the latter.
       (let [[orig-headers & orig-rows] (read-csv orig-tsv-file)
-            headers (vec (cons "corpus_cat" orig-headers))
-            rows    (vec (map #(vec (cons (str corpus "_" (first %)) %)) orig-rows))]
+            headers (cons "corpus_cat" orig-headers)
+            rows    (map #(cons (str corpus "_" (first %)) %) orig-rows)]
         (assert (= ["corpus_cat" "code" "name"] headers)
                 (str "Format error: Expected first line to contain column headers "
                      "and the two columns to be 'code' and 'name'."))
-        (csv/write-csv tsv-file (vec (cons headers rows)) :separator \tab)))))
+        (write-csv tsv-file (cons headers rows))))))
 
 (defn- modify-metadata-categories-config! [corpus config-path tsv-path]
   (spit config-path (-> metadata-categories-config-template
@@ -86,7 +116,13 @@
                         (cheshire/generate-string {:pretty true}))))
 
 
-(defn import-corpora []
+(defn- modify-tid-config! [corpus config-path tsv-path]
+  (spit config-path (-> tids-config-template
+                        (assoc-in [:source :file :path] tsv-path)
+                        (assoc-in [:transformers 2 :edge :joinValue] (str corpus "_tid"))
+                        (cheshire/generate-string {:pretty true}))))
+
+(defn import-corpora! []
   (let [tsv-path    (.getPath (io/resource "data/corpora.tsv"))
         config-path (fs/temp-file "corpus_config")]
     (modify-corpora-config! config-path tsv-path)
@@ -97,10 +133,20 @@
                      "and the two columns to be 'code' and 'name'."))
         (run-etl config-path)))))
 
-(defn import-metadata-categories [corpus]
+(defn import-metadata-categories! [corpus]
   (let [tsv-path    (.getPath (fs/temp-file "metadata-cats"))
         config-path (.getPath (fs/temp-file "metadata_cat_config"))]
     (modify-metadata-categories-tsv! corpus tsv-path)
     (modify-metadata-categories-config! corpus config-path tsv-path)
     (run-etl config-path)))
 
+(defn import-metadata-values! [corpus]
+  (let [tid-tsv-path      (.getPath (fs/temp-file "tids"))
+        other-tsv-path    (.getPath (fs/temp-file "metadata_vals"))
+        tid-config-path   (.getPath (fs/temp-file "tid_config"))
+        other-config-path (.getPath (fs/temp-file "metadata_val_config"))]
+    (println tid-config-path)
+    #_(create-tid-tsv! corpus tid-tsv-path)
+    (modify-tid-config! corpus tid-config-path tid-tsv-path)
+    #_(modify-metadata-values-tsv! corpus tsv-path)
+    #_(run-etl config-path)))
