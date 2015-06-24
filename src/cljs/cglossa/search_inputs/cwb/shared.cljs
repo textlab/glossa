70906e0656e2a1d4ad68feb4074a2f1d492afe5c
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-06-24 03:24:33 +0200

    Make row-removing buttons red

--- src/cljs/cglossa/search_inputs/cwb/shared.cljs
+++ src/cljs/cglossa/search_inputs/cwb/shared.cljs
@@ -9,11 +9,11 @@
     (search! query-cursor)))
 
 (defn remove-row-btn [show? handler]
-  [:button.btn.btn-default.btn-xs {:type     "button"
-                                   :title    "Remove row"
-                                   :on-click #(handler)
-                                   :style    {:margin-right 5
-                                              :visibility   (if show?
-                                                              "visible"
-                                                              "hidden")}}
+  [:button.btn.btn-danger.btn-xs {:type     "button"
+                                  :title    "Remove row"
+                                  :on-click #(handler)
+                                  :style    {:margin-right 5
+                                             :visibility   (if show?
+                                                             "visible"
+                                                             "hidden")}}
    [:span.glyphicon.glyphicon-remove]])
