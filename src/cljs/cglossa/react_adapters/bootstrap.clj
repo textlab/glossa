adc88b9fce00bf0cceb79d7fe791d69673e8a194
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-07-29 23:39:30 +0200

    Macro for adapting Boostrap classes to Reagent

---
+++ src/cljs/cglossa/react_adapters/bootstrap.clj
@@ -0,0 +1,13 @@
+(ns cglossa.react-adapters.bootstrap
+  (:require [clojure.string :as str]))
+
+;; This needs to be a macro in order for it to access the ReactBootstrap
+;; JS object in the js namespace (or is there another way?). It will define a set of Vars
+;; in the namespace where the macro is called, one for each of the listed Bootstrap
+;; components and resulting from running reagent.core/adapt-react-class
+;; on the component. The Var names will be lower-cased, for instance `button` for
+;; js/ReactBootstrap.Button.
+(defmacro adapt [components]
+  `(do ~@(for [c components]
+           `(def ~(symbol (str/lower-case c))
+              (reagent.core/adapt-react-class ~(symbol "js" (str "ReactBootstrap." c)))))))
---
+++ src/cljs/cglossa/react_adapters/bootstrap.cljs
@@ -0,0 +1,11 @@
+(ns cglossa.react-adapters.bootstrap
+  (:require cljsjs.react-bootstrap
+            reagent.core)
+  (:require-macros [cglossa.react-adapters.bootstrap :refer [adapt]]))
+
+;; This will define a set of Vars in the current namespace, one for each of the
+;; listed Bootstrap components and resulting from running reagent.core/adapt-react-class
+;; on the component. The Var names will be lower-cased, for instance `button` for
+;; js/ReactBootstrap.Button, so they should be referred like this in other namespaces:
+;; (:require [cglossa.react-adapters.bootstrap :refer [button modal label])
+(adapt ["Button" "Modal" "Label"])
