f91d768e09629293608eb6a32280e4b56c931287
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-07-31 10:49:32 +0200

    Use rest parameters in adapt! macro

--- src/cljs/cglossa/react_adapters/bootstrap.cljs
+++ src/cljs/cglossa/react_adapters/bootstrap.cljs
@@ -8,4 +8,4 @@
 ;; on the component. The Var names will be lower-cased, for instance `button` for
 ;; js/ReactBootstrap.Button, so they should be referred like this in other namespaces:
 ;; (:require [cglossa.react-adapters.bootstrap :refer [button modal label])
-(adapt! ["Button" "Modal" "Label"])
+(adapt! "Button" "Modal" "ModalHeader" "ModalTitle" "ModalBody" "ModalFooter" "Label")
