8d32faa7bb944439c786c2c7c5f39a99c0946da0
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-06 14:56:01 +0200

    Only show main area if corpus is non-nil

--- src/cljs/cglossa/app.cljs
+++ src/cljs/cglossa/app.cljs
@@ -6,13 +6,17 @@
 (defn- header []
   [b/navbar {:fixedTop true :brand "Glossa"}])
 
-(defn app [{:keys [showing-results?] :as a} {:keys [corpus] :as m}]
-  (let [cls (if (empty? (:metadata-categories @corpus)) "span12" "span9")]
-    [:div
-     [header]
-     [:div.container-fluid {:style {:padding-left 50}}
+(defn- main-area [{:keys [showing-results?] :as a} m]
+  [:div.container-fluid {:style {:padding-left 50}}
       [:div.row>div#main-content.col-md-12
        (if @showing-results?
          [results a m]
-         [start a m])]]
+         [start a m])]])
+
+(defn app [a {:keys [corpus] :as m}]
+  (let [cls (if (empty? (:metadata-categories @corpus)) "span12" "span9")]
+    [:div
+     [header]
+     (when @corpus
+       [main-area a m])
      [:div.app-footer>img.textlab-logo {:src "img/tekstlab.gif"}]]))
