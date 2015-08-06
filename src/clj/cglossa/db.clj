b0e13699706b9d6f75636a6c7c2b6c0816385fc7
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-06 14:55:23 +0200

    Include search engine in corpus hashmap

--- src/clj/cglossa/db.clj
+++ src/clj/cglossa/db.clj
@@ -99,7 +99,7 @@
 
 (defn get-corpus [code]
   (let [ress (sql-query (str "SELECT @rid as corpus_rid, name as corpus_name, "
-                             "logo, has_phonetic, has_headword_search, "
+                             "logo, search_engine, has_phonetic, has_headword_search, "
                              "$cats.@rid as cat_rids, $cats.code as cat_codes, "
                              "$cats.name as cat_names "
                              "FROM Corpus "
@@ -112,6 +112,7 @@
     {:corpus              {:rid                 (:corpus_rid res)
                            :name                (:corpus_name res)
                            :logo                (:logo res)
+                           :search-engine       (:search_engine res :cwb)
                            :has-phonetic        (:has_phonetic res)
                            :has-headword-search (:has_headword_search res)}
      :metadata-categories (-> (map (fn [rid name] {:rid rid :name name})
