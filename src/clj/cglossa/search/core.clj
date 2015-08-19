f3c7788c1f7f002e6c1741c32a1d78f7473ef421
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-19 19:55:35 +0200

    Add transform-results multimethod

--- src/clj/cglossa/search/core.clj
+++ src/clj/cglossa/search/core.clj
@@ -3,13 +3,18 @@
 
 (defmulti run-queries
   "Multimethod for actually running the received queries in a way that is
-  appropriate for the search engine of the corpus in question. The default
-  search engine is CWB, and hence the :default case is implemented in
-  cglossa.search.cwb."
-  (fn [corpus _ _]
-    (:search_engine corpus)))
+  appropriate for the search engine of the corpus in question."
+  (fn [corpus _ _] (keyword (:search_engine corpus))))
+
+(defmulti transform-results
+  "Multimethod for transforming search results in a way that is
+  appropriate for the search engine of the corpus in question."
+  (fn [corpus _] (keyword (:search_engine corpus))))
 
 (defn search [corpus-id queries]
-  (let [search      (db/run-sql "create vertex Search")
-        corpus      (first (db/sql-query "select from #TARGET" {:target corpus-id}))]
-    (run-queries corpus search queries)))
+  (let [search  (db/run-sql "create vertex Search")
+        corpus  (first (db/sql-query "select from #TARGET" {:target corpus-id}))
+        results (run-queries corpus search queries)]
+    (-> search
+        db/vertex->map
+        (assoc :results (transform-results corpus results))) ))
