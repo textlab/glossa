ce6a12599fad8a752515eb0bbdd9c3c4dda08e33
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-20 13:12:56 +0200

    Move function

--- src/clj/cglossa/search/cwb/speech.clj
+++ src/clj/cglossa/search/cwb/speech.clj
@@ -7,18 +7,6 @@
             [cglossa.search.cwb.shared :refer [cwb-query-name cwb-corpus-name
                                                construct-query-commands run-cqp-commands]]))
 
-(defn- fix-brace-positions [result]
-  ;; If the matching word/phrase is at the beginning of the segment, CQP puts the braces
-  ;; marking the start of the match before the starting segment tag
-  ;; (e.g. {{<turn_endtime 38.26><turn_starttime 30.34>went/go/PAST>...). Probably a
-  ;; bug in CQP? In any case we have to fix it by moving the braces to the
-  ;; start of the segment text instead. Similarly if the match is at the end of a segment.
-  (-> result
-      (str/replace #"\{\{((?:<\S+?\s+?\S+?>\s*)+)"          ; Find start tags with attributes
-                   "\1{{")                                  ; (i.e., not the match)
-      (str/replace #"((?:</\S+?>\s*)+)\}\}"                 ; Find end tags
-                   "}}\1")))
-
 (defmethod run-queries :cwb_speech [corpus search queries]
   (let [search-id   (db/stringify-rid search)
         named-query (cwb-query-name corpus search-id)
@@ -31,6 +19,18 @@
                      "show +sync_time"
                      "cat Last"]]
     (run-cqp-commands (flatten commands))))
+
+(defn- fix-brace-positions [result]
+  ;; If the matching word/phrase is at the beginning of the segment, CQP puts the braces
+  ;; marking the start of the match before the starting segment tag
+  ;; (e.g. {{<turn_endtime 38.26><turn_starttime 30.34>went/go/PAST>...). Probably a
+  ;; bug in CQP? In any case we have to fix it by moving the braces to the
+  ;; start of the segment text instead. Similarly if the match is at the end of a segment.
+  (-> result
+      (str/replace #"\{\{((?:<\S+?\s+?\S+?>\s*)+)"          ; Find start tags with attributes
+                   "\1{{")                                  ; (i.e., not the match)
+      (str/replace #"((?:</\S+?>\s*)+)\}\}"                 ; Find end tags
+                   "}}\1")))
 
 (defn- find-timestamps [result]
   (for [[segment start end] (re-seq #"<sync_time\s+([\d\.]+)><sync_end\s+([\d\.]+)>.*?</sync_time>"
