fbafd8a67aa7c7d53cc9e7e1cfdde4f9bd3fdc99
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-30 14:51:53 +0200

    Update ClojureScript

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :test-paths ["spec/clj"]
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.48" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.5"]
