26eec9997c0eb2e753c3a5389d479dd2636aa883
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-08-31 13:09:52 +0200

    WIP on metadata select lists

--- src/cljs/cglossa/left.cljs
+++ src/cljs/cglossa/left.cljs
@@ -1,8 +1,11 @@
 (ns cglossa.left
-  (:require react-select))
+  (:require [reagent.core :as r]
+            react-select))
 
 (defn metadata-list [_ {:keys [metadata-categories]}]
-  [:div#sidebar-wrapper
-   [:ul.sidebar-nav
-    [:li ]
-    [:li [:a {:href "#"} "Some text"]]]])
+  (let [select (r/adapt-react-class js/Select)]
+    [:div#sidebar-wrapper
+     (for [cat @metadata-categories]
+       [select {:multi true
+                :options   [{:value "one" :label "One"} {:value "two" :label "Two"}]
+                :on-change #(.log js/console "endret")}])]))
