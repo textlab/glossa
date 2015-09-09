26aaebb086303e6df56db02b64501fec3b6f8726
Michał Kosek <mihao@users.noreply.github.com>
2015-09-09 19:33:59 +0200

    Prepare to merge with the main glossa repo

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JVM_OPTS -cp target/cglossa.jar clojure.main -m cglossa.server
--- src/clj/cglossa/search_engines.clj
+++
@@ -1,6 +0,0 @@
-(ns cglossa.search-engines
-  "Require namespaces containing implementations for various search engines
-  here in order to register any multimethod implementations found in those
-  namespaces."
-  (:require cglossa.search.cwb.written
-            cglossa.search.cwb.speech))
