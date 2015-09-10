37d5f93e2d191e42f19630171912fd9fe73e7c14
Anders Nøklestad <anders.noklestad@iln.uio.no>
2015-09-10 16:14:18 +0200

    Change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cglossa "0.1.0-SNAPSHOT"
+(defproject rglossa "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
